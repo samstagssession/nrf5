f93de77f868a82b28d9e6fd452951ed25ae4679f
andy <samstagssession@gmail.com>
2018-10-03 18:36:21 +0200

    added my_projects/ble_app_beacon

---
+++ nRF5_SDK_12.3.0_d7731ad/my_projects/blinky/generic_nrf51822/blank/armgcc/_build/nrf51822_xxaa_boards.c.d
@@ -0,0 +1,149 @@
+_build/nrf51822_xxaa_boards.c.o: \
+ ../../../../../components/boards/boards.c \
+ ../../../../../components/boards/boards.h \
+ ../../../../../components/drivers_nrf/hal/nrf_gpio.h \
+ ../../../../../components/device/nrf.h \
+ ../../../../../components/device/nrf51.h \
+ ../../../../../components/toolchain/cmsis/include/core_cm0.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/stdint.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/_default_types.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/features.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/_newlib_version.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_intsup.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../components/toolchain/system_nrf51.h \
+ ../../../../../components/device/nrf51_bitfields.h \
+ ../../../../../components/device/nrf51_deprecated.h \
+ ../../../../../components/device/compiler_abstraction.h \
+ ../../../../../components/drivers_nrf/hal/nrf_peripherals.h \
+ ../../../../../components/device/nrf51822_peripherals.h \
+ ../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../components/libraries/util/app_error.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/stdio.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/_ansi.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/newlib.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/config.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/ieeefp.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/cdefs.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stddef.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdarg.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/reent.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/_ansi.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_types.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/_types.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/lock.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/types.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/types.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/stdio.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h \
+ ../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../config/sdk_config.h \
+ ../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../components/libraries/util/app_error_weak.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/stdlib.h \
+ /home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/stdlib.h \
+ ../../../../../components/boards/pca10028.h
+
+../../../../../components/boards/boards.h:
+
+../../../../../components/drivers_nrf/hal/nrf_gpio.h:
+
+../../../../../components/device/nrf.h:
+
+../../../../../components/device/nrf51.h:
+
+../../../../../components/toolchain/cmsis/include/core_cm0.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/stdint.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/_default_types.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/features.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/_newlib_version.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_intsup.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../components/toolchain/system_nrf51.h:
+
+../../../../../components/device/nrf51_bitfields.h:
+
+../../../../../components/device/nrf51_deprecated.h:
+
+../../../../../components/device/compiler_abstraction.h:
+
+../../../../../components/drivers_nrf/hal/nrf_peripherals.h:
+
+../../../../../components/device/nrf51822_peripherals.h:
+
+../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../components/libraries/util/app_error.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/stdio.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/_ansi.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/newlib.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/config.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/cdefs.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stddef.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdarg.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/reent.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/_ansi.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_types.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/_types.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/lock.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/types.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/types.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/sys/stdio.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h:
+
+../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../config/sdk_config.h:
+
+../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../components/libraries/util/app_error_weak.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/stdlib.h:
+
+/home/test/nrf5/gcc-arm-none-eabi-7-2018-q2-update/arm-none-eabi/include/machine/stdlib.h:
+
+../../../../../components/boards/pca10028.h:
